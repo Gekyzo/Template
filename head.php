3337b05329c00f5131011f9aa87b058d3b33f2e0
Ciro Mora <cirocansino@hotmail.com>
2016-12-26 21:01:25 +0100

    Add files via upload
    - Name of .css file updated to main.css
    - Link on head.php file updated to the new .css file

---
+++ head.php
@@ -0,0 +1,12 @@
+<?php
+	$root = $_SERVER['DOCUMENT_ROOT'];
+	include $root . "/php/connect.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link type="text/css" rel="stylesheet" media="all" href="/css/normalize.css" />
+	<link type="text/css" rel="stylesheet" media="all" href="/css/main.css" />
+	<script src="/js/jquery.js"></script>
