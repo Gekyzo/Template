d07a480b90f8d75f1c95f0f4c00e729fc295bad5
Ciro Mora <cirocansino@hotmail.com>
2017-10-26 18:05:31 +0200

    Cambios en Head

--- php/head.php
+++ php/head.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
 	<meta charset="utf-8" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!-- Favicon -->
+	<link rel="shortcut icon" type="image/ico" href="/favicon.ico" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+	<!-- CSS -->
+	<link rel="stylesheet" media="all" href="/css/main.css" />
+	<!-- SEO -->
+	<meta name="robots" content="index,follow">
+	<meta name="description" content="">
 	<meta property="og:title" content="">
 	<meta property="og:url" content="">
-	<link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
-	<link rel="stylesheet" media="all" href="/css/normalize.min.css" />
-	<link rel="stylesheet" media="all" href="/css/main.css" />
+	<link rel="canonical" href="">
+	<title></title>
 
 <?php
 	define ("root", $_SERVER['DOCUMENT_ROOT']);
